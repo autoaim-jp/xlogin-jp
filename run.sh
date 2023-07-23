13d32273de67ae7c222b91195e63bf28aebc5506
autoaim_jp <git@autoaim.jp>
2023-07-23 09:48:29 +0900

    fix: cat test.log

--- run.sh
+++ run.sh
@@ -29,7 +29,9 @@ if [ $fileId = "test" ] && [ $op = "up" ] ; then
   docker compose -p ${projectName} -f ${dockerComposeFile} up > test.log &
   sleep 30
   docker compose -p ${projectName} -f ${dockerComposeFile} down >> test.log &
+
   # grepでログファイルを検索した結果、successという文字列がなければ終了コード1で終了
+  cat test.log
   cat test.log | grep "success"
 elif [ $op = "clean" ] ; then
   docker compose -p ${projectName} -f ${dockerComposeFile} down
