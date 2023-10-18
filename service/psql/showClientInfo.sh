d006dbcb372ce40f14407972028c52b8f16fcb50
autoaim_jp <git@autoaim.jp>
2023-10-19 01:05:42 +0900

    fix: Dockerfile path

--- service/psql/showClientInfo.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-docker exec -it xlapp-container-psql psql -U xl_admin xl_db -c "select * from access_info.client_list;"
-docker exec -it xlapp-container-psql psql -U xl_admin xl_db -c "select * from access_info.secret_list;"
-
