7272da6099db67e4ff86eb336d443e45c8d10f14
autoaim_jp <git@autoaim.jp>
2024-03-27 01:40:29 +0900

    add: tesseractApi container

---
+++ service/tesseractApi/src/lib/index.js
@@ -0,0 +1,41 @@
+import backendServerLib from './backendServerLib.js'
+/**
+ * @file
+ * @name アプリケーション全体で共通で使用するライブラリ
+ * @memberof lib
+ */
+const mod = {}
+
+/**
+ * init.
+ *
+ * @param {} crypto
+ * @param {} ulid
+ * @return {undefined} 戻り値なし
+ * @memberof lib
+ */
+const init = ({ crypto, ulid, winston }) => {
+  mod.crypto = crypto
+  mod.ulid = ulid
+  backendServerLib.init({ crypto, ulid, winston })
+}
+
+
+const createAmqpConnection = async ({
+  amqplib, user, pass, host, port,
+}) => {
+  const conn = await amqplib.connect(`amqp://${user}:${pass}@${host}:${port}`)
+  return conn
+}
+
+const getUlid = () => {
+  return mod.ulid()
+}
+
+export default {
+  backendServerLib,
+  init,
+  createAmqpConnection,
+  getUlid,
+}
+
---
+++ service/tesseractApi/src/setting/index.js
@@ -0,0 +1,62 @@
+import browserServerSetting from './browserServerSetting.js'
+
+const setting = {}
+
+const init = ({ env }) => {
+  setting.env = {}
+  setting.env.SERVER_ORIGIN = env.SERVER_ORIGIN
+  setting.env.SESSION_SECRET = env.SESSION_SECRET
+  setting.env.SERVER_PORT = env.SERVER_PORT
+  setting.env.SERVICE_NAME = env.SERVICE_NAME
+
+  setting.env.AMQP_USER = env.AMQP_USER
+  setting.env.AMQP_PASS = env.AMQP_PASS
+  setting.env.AMQP_HOST = env.AMQP_HOST
+  setting.env.AMQP_PORT = env.AMQP_PORT
+}
+
+setting.amqp = {}
+setting.amqp.TESSERACT_REQUEST_QUEUE = 'tesseract-request-queue'
+setting.amqp.TESSERACT_RESPONSE_QUEUE = 'tesseract-response-queue'
+
+setting.url = {}
+setting.url.API_VERSION = 'v0.1.0'
+setting.url.ERROR_PAGE = '/error'
+setting.url.AFTER_CONNECT = '/login'
+setting.url.AFTER_CHECK_CREDENTIAL = '/confirm'
+
+
+const getList = (...keyList) => {
+  /* eslint-disable no-param-reassign */
+  const constantList = keyList.reduce((prev, key) => {
+    let value = setting
+    for (const keySplit of key.split('.')) {
+      value = value[keySplit]
+    }
+    prev[key.slice(key.lastIndexOf('.') + 1)] = value
+    return prev
+  }, {})
+  for (const key of keyList) {
+    if (constantList[key.slice(key.lastIndexOf('.') + 1)] === undefined) {
+      throw new Error(`[error] undefined setting constant: ${key}`)
+    }
+  }
+  return constantList
+}
+
+
+const getValue = (key) => {
+  let value = setting
+  for (const keySplit of key.split('.')) {
+    value = value[keySplit]
+  }
+  return value
+}
+
+export default {
+  browserServerSetting,
+  init,
+  getList,
+  getValue,
+}
+
